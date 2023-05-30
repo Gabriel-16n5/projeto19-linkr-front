cda5bc1a502a08adcc16771ff93a4659d97673ed
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-05-30 13:55:17 -0300

    minor corrections

--- src/Pages/SigninPage/SigninPage.js
+++ src/Pages/SigninPage/SigninPage.js
@@ -1,12 +1,12 @@
 import { useEffect } from 'react';
 
-export default function SignupPage () {
+export default function SigninPage () {
   
     useEffect(() => {
 
     }, []);
   
     return (
-        <></>
+        <><p>oi</p></>
     );
   }
--- src/Pages/SignupPage/SignupPage.js
+++ src/Pages/SignupPage/SignupPage.js
@@ -7,6 +7,6 @@ export default function SignupPage () {
     }, []);
   
     return (
-        <></>
+        <><p>oiii</p></>
     );
   }
