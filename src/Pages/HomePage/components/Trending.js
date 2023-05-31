a618b26d208ec0fbab4905a07d4de03b5fe8d1a1
GustavoOlimpio <gustavo-olimpio@hotmail.com>
2023-05-31 09:19:16 -0300

    feat/trendings

--- src/Pages/HomePage/components/Trending.js
+++ src/Pages/HomePage/components/Trending.js
@@ -1,15 +1,44 @@
 import styled from "styled-components"
 
 export default function Trending(){
+    const hashtags = ["javascript","react","react-native","material","web-dev","mobile","css","html","node","sql"]
     return(
-        <Main></Main>
+        <Main>
+            <h1>trending</h1>
+            <section></section>
+            {hashtags.map(a=> <p># {a}</p>)}
+        </Main>
     )
         
     }
     
     const Main=styled.div`
     height:406px;
-    width:301px;
+    width:100%;
     background-color:#171717;
     border-radius:16px;
+    padding-top:12px;
+    color:#FFFFFF;
+    h1{
+        
+        font-family:'Oswald', sans-serif;
+        font-weight:700;
+        font-size:27px;
+        margin-left:16px;
+        
+    }
+    section{
+            width:100%;
+            height:1px;
+            background-color:#484848;
+            margin-top:12px;
+            margin-bottom:22px;
+    }
+    p{
+        font-family: 'Lato', sans-serif;
+        font-size:19px;
+        font-weight:700;
+        margin-left:16px;
+        margin-bottom:12px;
+    }
     `
