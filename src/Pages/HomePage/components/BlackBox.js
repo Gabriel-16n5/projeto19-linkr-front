07c20256d929112d598e1b92eb1557d2e3cb2e5f
Victor Mafra <victorvmafra97@gmail.com>
2023-05-31 12:28:43 -0300

    feat/BlackBox

--- src/Pages/HomePage/components/BlackBox.js
+++ src/Pages/HomePage/components/BlackBox.js
@@ -8,10 +8,15 @@ return(
     <Main>
         <img src="https://conteudo.imguol.com.br/c/esporte/d0/2023/05/03/haaland-comemora-gol-marcado-durante-manchester-city-x-west-ham-pelo-campeonato-ingles-1683146420962_v2_450x600.jpg" alt=""/>
         <TextContainer>
-            <p>Name</p>
-            <span>Text</span>
+            <p>Haalandinho Gaúcho</p>
+            <span>Muito maneiro esse tutorial de Material UI com React, deem uma olhada!</span>
             <UrlContainer>
-              url infos
+                <UrlTextContainer>
+                    <h1>Como aplicar o Material UI em um projeto React</h1>
+                    <p>Hey! I have moved this tutorial to my personal blog. Same content, new location. Sorry about making you click through to another page.</p>
+                    <span>https://medium.com/@pshrmn/a-simple-react-router</span>
+                </UrlTextContainer>
+                <img src="https://img.freepik.com/free-icon/atomic-energy_318-914621.jpg?size=626&ext=jpg&uid=R104496286&ga=GA1.2.1934028619.1685108812&semt=sph"/>
             </UrlContainer>
 
 
@@ -59,8 +64,45 @@ const TextContainer=styled.div`
 `
 
 const UrlContainer=styled.div`
-  width: 82%;
-  height: 56%;
-  border: 1px solid #4D4D4D;
-  border-radius: 11px;
+    width: 88%;
+    height: 56%;
+    border: 1px solid #4D4D4D;
+    border-radius: 11px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 15px;
+    img {
+        height: 100%;
+        width: 30%;
+        object-fit: contain;
+    }
+`
+
+const UrlTextContainer=styled.div`
+    width: 61%;
+    height: 100%;
+
+    font-family: 'Lato', sans-serif;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    h1 {
+        font-size: 16px;
+        font-weight: 400;
+        margin-bottom: 0;
+        color: #CECECE;
+    }
+    p {
+        font-family: 'Lato', sans-serif;
+        font-weight: 400;
+        font-size: 11px;
+        color: #9B9595;
+    }
+    span {
+        font-family: 'Lato', sans-serif;
+        font-weight: 400;
+        font-size: 11px;
+        color: #CECECE;
+    }
 `
