cb12e2cdaadf999f76cc07425c0b3da71d01f4d9
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-05-31 14:08:25 -0300

    feat/signup Done

--- src/Pages/SignupPage/SignupPage.js
+++ src/Pages/SignupPage/SignupPage.js
@@ -1,17 +1,34 @@
-import { useEffect } from 'react';
+import React from "react"
 import { MainContainer, LeftContainer, RightContainer, Container, Form, Input, Button, Message } from './StyledSinupPage';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios"
 
 export default function SignupPage () {
-  
-    useEffect(() => {
-
-    }, []);
-  
-        const handleSubmit = (event) => {
-          event.preventDefault();
-          // Lógica para fazer o login aqui
-        };
+    const [wait, setWait] = React.useState(false);
+    const [email, setEmail] = React.useState([]);
+    const [password, setPassword] = React.useState([]);
+    const [username, setUsername] = React.useState([]);
+    const [url, setUrl] = React.useState([]);
+    const navigate = useNavigate();
+    
+    function register(e){
+       e.preventDefault();
+        setWait(!wait)
+        const body = {
+            email: `${email}`,
+            username: `${username}`,
+            password: `${password}`,
+            pictureUrl: `${url}`
+        }
+        console.log(body);
+        const promise = axios.post(`${process.env.REACT_APP_API_URL}/signup`, body)
+       promise.then((ok) => navigate("/signin"));
+       promise.catch((erro) => {
+        alert(erro.message);
+        setWait(false);
+        console.log(erro.message)
+       });
+    }
 
     return (
         <MainContainer>
@@ -21,11 +38,35 @@ export default function SignupPage () {
             </LeftContainer>
             <RightContainer>
                 <Container>
-                    <Form onSubmit={handleSubmit}>
-                        <Input type="text" placeholder="e-mail" />
-                        <Input type="password" placeholder="password" />
-                        <Input type="text" placeholder="username" />
-                        <Input type="text" placeholder="picture url" />
+                    <Form onSubmit={register}>
+                        <Input 
+                        type="text" 
+                        placeholder="e-mail" 
+                        disabled={wait}
+                        value={email}
+                        onChange={(e) => {setEmail(e.target.value)}}
+                        />
+                        <Input 
+                        type="password" 
+                        placeholder="password" 
+                        disabled={wait}
+                        value={password}
+                        onChange={(e) => {setPassword(e.target.value)}}
+                        />
+                        <Input 
+                        type="text" 
+                        placeholder="username" 
+                        disabled={wait}
+                        value={username}
+                        onChange={(e) => {setUsername(e.target.value)}}
+                        />
+                        <Input 
+                        type="text" 
+                        placeholder="picture url" 
+                        disabled={wait}
+                        value={url}
+                        onChange={(e) => {setUrl(e.target.value)}}
+                        />
                         <Button type="submit">Sign Up</Button>
                     </Form>
                     <Message>
