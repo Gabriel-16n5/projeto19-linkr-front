fe201e095a97dcd04968ff8b8ad06f501bc74ace
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-05-30 22:33:52 -0300

    feat/login

--- src/Pages/SigninPage/SigninPage.js
+++ src/Pages/SigninPage/SigninPage.js
@@ -1,4 +1,6 @@
 import { useEffect } from 'react';
+import { MainContainer, LeftContainer, RightContainer, Container, Form, Input, Button, Message } from './StyledSigninPage';
+import { Link } from 'react-router-dom';
 
 export default function SigninPage () {
   
@@ -6,7 +8,29 @@ export default function SigninPage () {
 
     }, []);
   
+        const handleSubmit = (event) => {
+          event.preventDefault();
+          // Lógica para fazer o login aqui
+        };
+
     return (
-        <><p>oi</p></>
+        <MainContainer>
+            <LeftContainer>
+                <h1>linkr</h1>
+                <h3>save, share and discover the best links on the web</h3>
+            </LeftContainer>
+            <RightContainer>
+                <Container>
+                    <Form onSubmit={handleSubmit}>
+                        <Input type="text" placeholder="e-mail" />
+                        <Input type="password" placeholder="password" />
+                        <Button type="submit">Log in</Button>
+                    </Form>
+                    <Message>
+                        <Link to="/">First time? Create an account!</Link>
+                    </Message>
+                </Container>
+            </RightContainer>
+        </MainContainer>
     );
   }
