397c6fcf62963c616698013cb960c478dfb08c54
curtyraissa <raissacurty@gmail.com>
2023-06-01 18:24:44 -0300

    ajuste

--- src/Pages/HomePage/StyledHomePage.js
+++ src/Pages/HomePage/StyledHomePage.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Main = styled.div`
   background-color: #333333;
   width: 100%;
-  height: 100%;
+  height: 100vh;
   display: flex;
   justify-content: center;
 `;
