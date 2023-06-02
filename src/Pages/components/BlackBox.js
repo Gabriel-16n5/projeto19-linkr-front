b3fac9a4adec1b48271fab6c57731b545394e23c
Victor Mafra <victorvmafra97@gmail.com>
2023-06-02 17:42:25 -0300

    added text edit and people name likes

--- src/Pages/components/BlackBox.js
+++ src/Pages/components/BlackBox.js
@@ -1,72 +1,125 @@
 import styled from 'styled-components';
 import { IoIosHeartEmpty, IoIosHeart, } from 'react-icons/io';
 import { BsPencilSquare, BsFillTrashFill } from 'react-icons/bs';
-import { useContext, useRef, useState } from 'react';
+import { useContext, useRef, useState, useEffect } from 'react';
 import { TimelineContext } from '../../contexts/TimelineContext';
-import {Tooltip} from "react-tooltip";
+import { Tooltip } from "react-tooltip";
+import axios from 'axios';
 
 export default function BlackBox(props) {
-  const [filled, setFilled] = useState(false);
-  // const { deleted, setDeleted, open, setOpen } = useContext(TimelineContext);
-  const { setDeleted } = useContext(TimelineContext);
-  const inputRef = useRef(null);
+    const [filled, setFilled] = useState(false);
+    // const { deleted, setDeleted, open, setOpen } = useContext(TimelineContext);
+    const { setDeleted } = useContext(TimelineContext);
+    const [isEditing, setIsEditing] = useState(false)
+    const [text, setText] = useState(props.text)
+    const textRef = useRef(null);
+    const inputRef = useRef(null);
+    const peopleNumberLikes = props.peopleLike.length
+    const peopleLikes = props.peopleLike
 
-  function handleClick() {
-    setFilled(!filled);
-  }
+    console.log(props.token)
 
-  function deletePost() {
-    setDeleted(true);
-  }
-  function clickLink(){
-    const url = props.url
-    window.open(url, "_blank");
-  }
-  //  usar url-metadata no back-end
+    function namePeopleLike(){
+        if(peopleNumberLikes === 0) return "No Likes"
+        else if (peopleNumberLikes === 1) return `${peopleLikes[0].username} curtiu`
+        else if (peopleNumberLikes === 2) return `${peopleLikes[0].username} e ${peopleLikes[1].username} curtiram`
+        else return `${peopleLikes[0].username}, ${peopleLikes[1].username} e mais ${Number(peopleNumberLikes)-2}curtiram`
+    }
 
-  return (
-    <Main >
-      <ImageLikesContainer>
-        <img
-          src="https://conteudo.imguol.com.br/c/esporte/d0/2023/05/03/haaland-comemora-gol-marcado-durante-manchester-city-x-west-ham-pelo-campeonato-ingles-1683146420962_v2_450x600.jpg"
-          alt="imagem perfil"
-        />
-        <HeartIcon
-          filled={filled}
-          onClick={handleClick}
-          data-tooltip-id="my-tooltip"
-          data-tooltip-content={`Curtido por uma galera`}
-        >
-          {filled ? <IoIosHeart size={24} /> : <IoIosHeartEmpty size={24} />}
-        </HeartIcon>
-        <Tooltip id="my-tooltip" placement="bottom" />
-        {/* ao passar o mouse por cima <p>Ederson, Kevin e outras 39 pessoas</p> */}
-        <p>39 likes</p>
-      </ImageLikesContainer>
-      <TextContainer>
-        <TextTopContainer>
-          <p>{props.name}</p>
-          <IconsContainer>
-            <BsPencilSquare size={20} />
-            <BsFillTrashFill size={20} onClick={deletePost} />
-          </IconsContainer>
-        </TextTopContainer>
-        <span>
-          {props.text}
-        </span>
-        <UrlContainer onClick={clickLink}>
-          <UrlTextContainer>
-            <h2>{props.title}</h2>
-            <p>
-              {props.description}
-            </p>
-            <span>{props.url}</span>
-          </UrlTextContainer>
-          <img src={props.image} alt="imagem site"/>
-        </UrlContainer>
-      </TextContainer>
-    </Main>
-  );
+    function clickEditing() {
+        setIsEditing(!isEditing)
+    }
+
+    function keyPress(e) {
+        if (e.key === 'Enter') {
+            setIsEditing(false)
+            const newText = inputRef.current.value
+            axios.put(`${process.env.REACT_APP_API_URL}/timeline/${props.postId}`, { newText }, {headers: {
+                'Authorization': `Bearer ${props.token}`
+              }})
+    
+            .then(response => {
+                setText(newText)
+                console.log(response.message)
+            })
+            .catch(error => {
+                console.log(error.message)
+                alert("It wasn't able to save new change")
+            });
+
+        } else if (e.key === 'Escape') {
+            setIsEditing(false)
+        }
+    }
+
+    function fillHeart() {
+        setFilled(!filled);
+    }
+
+    function deletePost() {
+        setDeleted(true);
+    }
+    function clickLink() {
+        const url = props.url
+        window.open(url, "_blank");
+    }
+    
+    useEffect(() => {
+        if (isEditing) {
+          inputRef.current.focus();
+        }
+      }, [isEditing]);
+
+    return (
+        <Main >
+            <ImageLikesContainer>
+                <img
+                    src="https://conteudo.imguol.com.br/c/esporte/d0/2023/05/03/haaland-comemora-gol-marcado-durante-manchester-city-x-west-ham-pelo-campeonato-ingles-1683146420962_v2_450x600.jpg"
+                    alt="imagem perfil"
+                />
+                <HeartIcon
+                    data-tooltip-id="my-tooltip"
+                    data-tooltip-content={namePeopleLike()}
+                    data-tooltip-place="bottom"
+                    onClick={fillHeart}
+                    filled={filled}>
+                    {filled ? <IoIosHeart size={24} /> : <IoIosHeartEmpty size={24} />}
+                </HeartIcon>
+                <ReactToolTip id="my-tooltip" />
+                {/* ao passar o mouse por cima <p>Ederson, Kevin e outras 39 pessoas</p> */}
+                <p>{`${peopleNumberLikes} likes`}</p>
+            </ImageLikesContainer>
+            <TextContainer>
+                <TextTopContainer>
+                    <p>{props.name}</p>
+                    <IconsContainer>
+                        <BsPencilSquare size={20} onClick={clickEditing} />
+                        <BsFillTrashFill size={20} onClick={deletePost} />
+                    </IconsContainer>
+                </TextTopContainer>
+                {isEditing ? (
+                    <input
+                        ref={inputRef}
+                        type="text"
+                        defaultValue={text}
+                        onKeyDown={keyPress}
+                    />
+                ) : (
+                    <span ref={textRef}>{text}</span>
+                )}
+                <UrlContainer onClick={clickLink}>
+                    <UrlTextContainer>
+                        <h2>{props.title}</h2>
+                        <p>
+                            {props.description}
+                        </p>
+                        <span>{props.url}</span>
+                    </UrlTextContainer>
+                    <img src={props.image} alt="imagem site" />
+                </UrlContainer>
+            </TextContainer>
+        </Main>
+    );
 }
 
 const Main = styled.div`
@@ -194,13 +247,8 @@ const IconsContainer = styled.div`
   justify-content: space-between;
 `;
 
-const ReactTooltip = styled(Tooltip)`
-  &.type-dark.place-top {
-    background-color: blue;
-    padding: 0.3rem 1rem;
-
-    &:after {
-      border-top-color: blue;
-    }
-  }
-`;
+const ReactToolTip = styled(Tooltip)`
+    font-family: "Lato", sans-serif;
+    font-weight: 400;
+    font-size: 12px;
+`
