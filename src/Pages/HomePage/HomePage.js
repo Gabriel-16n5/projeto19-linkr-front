265024bb7ed6615f9a29efdfab569a5fc16d75a8
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-06-08 15:03:26 -0300

    fix:HUB avaliador

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -130,7 +130,6 @@ export default function HomePage() {
               <YesButton data-test="confirm" onClick={yesDelete}>Yes, delete it</YesButton>
             </ButtonsContainer>
           </DeletedContainer>
-
           <BackgroundOpacity />
         </>
       ) : (
